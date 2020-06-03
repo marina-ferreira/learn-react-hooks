6d2e9f5dc798cedfbc49130863bee6da70cdef46
Marina Ferreira <marina.goncalves@locaweb.com.br>
2020-06-03 06:36:52 -0300

    Implements registration logic

--- chapter_3/src/user/Register.js
+++ chapter_3/src/user/Register.js
@@ -1,18 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Register = () => {
+const Register = ({ setUser }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
+
+  const handleUsername = e => {
+    setUsername(e.target.value)
+  }
+
+  const handlePassword = e => {
+    setPassword(e.target.value)
+  }
+
+  const handlePasswordConfirm = e => {
+    setPasswordConfirm(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    setUser(username)
+  }
+
   return (
-    <form onSubmit={e => e.preventDefault()}>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="register-username">Username</label>
-      <input type="text" name="register-username" id="register-username" />
+      <input
+        type="text"
+        name="register-username"
+        id="register-username"
+        value={username}
+        onChange={handleUsername}
+      />
 
       <label htmlFor="register-password">Password</label>
-      <input type="password" name="register-password" id="register-password" />
+      <input
+        type="password"
+        name="register-password"
+        id="register-password"
+        value={password}
+        onChange={handlePassword}
+      />
 
       <label htmlFor="register-password-confirm">Confirm Password</label>
-      <input type="password" name="register-password-confirm" id="register-password-confirm" />
+      <input
+        type="password"
+        name="register-password-confirm"
+        id="register-password-confirm"
+        value={passwordConfirm}
+        onChange={handlePasswordConfirm}
+      />
 
-      <input type="submit" value="Register" />
+      <input
+        type="submit"
+        value="Register"
+        disabled={
+          username.length === 0 ||
+          password.length === 0 ||
+          password !== passwordConfirm
+        }
+      />
     </form>
   )
 }
