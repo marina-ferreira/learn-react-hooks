8e77be2119392b8742dc8e64ca96aa063b9ff332
Marina Ferreira <marina.goncalves@locaweb.com.br>
2020-06-09 08:28:40 -0300

    Combines user and posts reducer into one

--- chapter_4/src/App.js
+++ chapter_4/src/App.js
@@ -1,45 +1,20 @@
 import React, { useReducer } from 'react'
+
 import UserBar from 'user/UserBar'
 import CreatePost from 'post/CreatePost'
 import PostList from 'post/PostList'
+import appReducer from 'reducers'
 
 import './App.css'
 
-const userReducer = (state, action) => {
-  switch (action.type) {
-    case 'LOGIN':
-    case 'REGISTER':
-      return action.username
-    case 'LOGOUT':
-      return ''
-    default:
-      throw new Error()
-  }
-}
-
-const postsReducer = (state, action) => {
-  switch(action.type) {
-    case 'CREATE_POST':
-      const newPost = {
-        title: action.title,
-        content: action.content,
-        author: action.author
-      }
-
-      return [newPost, ...state]
-    default:
-      throw new Error()
-  }
-}
-
 function App() {
-  const [user, dispatchUser] = useReducer(userReducer, '')
-  const [posts, dispatchPosts] = useReducer(postsReducer, defaultPosts)
+  const [state, dispatch] = useReducer(appReducer, { user: '', posts: defaultPosts })
+  const { user, posts } = state
 
   return (
     <div className="App">
-      <UserBar user={user} dispatch={dispatchUser} />
-      {user && <CreatePost user={user} posts={posts} dispatch={dispatchPosts} />}
+      <UserBar user={user} dispatch={dispatch} />
+      {user && <CreatePost user={user} posts={posts} dispatch={dispatch} />}
 
       <PostList posts={posts} />
     </div>
