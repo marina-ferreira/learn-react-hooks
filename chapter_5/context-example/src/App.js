24e578951c9f98460880e5e678887f595beaf7bc
Marina Ferreira <marina.goncalves@locaweb.com.br>
2020-06-12 19:47:00 -0300

    Chapter 5 context example | provider

--- chapter_5/context-example/src/App.js
+++ chapter_5/context-example/src/App.js
@@ -3,10 +3,19 @@ import Header from 'components/Header'
 
 function App() {
   return (
-    <Header text="Hello World" />
+    <ThemeContext.Provider value={{ primaryColor: 'coral' }}>
+      <Header text="Hello World" />
+
+      <ThemeContext.Provider value={{ primaryColor: 'deeppink' }}>
+        <Header text="Hello Planet" />
+      </ThemeContext.Provider>
+    </ThemeContext.Provider>
   )
 }
 
 export default App
 
 export const ThemeContext = createContext({ primaryColor: 'deepskyblue' })
+
+// Contexts use the default value that is passed to createContext
+// when there is no provider defined.
