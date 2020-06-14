6d925b489df1389ad1e2a27a5059e8f30312292a
Marina Ferreira <marina.goncalves@locaweb.com.br>
2020-06-14 08:40:16 -0300

    Chapter 6 | Wraps App component inside RequestProvider

--- chapter_6/blog/src/index.js
+++ chapter_6/blog/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { RequestProvider } from 'react-request-hook'
+import axios from 'axios'
+
 import App from './App'
 
+const api = axios.create({
+  baseURL: 'http://localhost:3000/api'
+})
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RequestProvider value={api}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </RequestProvider>,
   document.getElementById('root')
 )
--- chapter_6/blog/src/setupProxy.js
+++ chapter_6/blog/src/setupProxy.js
@@ -4,5 +4,6 @@ module.exports = function (app) {
   app.use(createProxyMiddleware('/api', {
     target: 'http://localhost:4000',
     pathRewrite: { '^/api' : '' }
+    // removes the /api prefix before forwarding the request to the server
   }))
 }
