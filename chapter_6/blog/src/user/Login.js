ffa759d8d1e188b0be67bd436be4c978bfe632e1
Marina Ferreira <marina.goncalves@locaweb.com.br>
2020-06-14 11:20:47 -0300

    Chapter 6 | User login with useResource

--- chapter_6/blog/src/user/Login.js
+++ chapter_6/blog/src/user/Login.js
@@ -1,17 +1,43 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import { StateContext } from 'contexts'
+import { useResource } from 'react-request-hook'
 
 const Login = () => {
   const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [loginFailed, setLoginFailed] = useState(false)
   const { dispatch } = useContext(StateContext)
+  const [user, login] = useResource((username, password) => ({
+    url: `/login/${encodeURI(username)}/${encodeURI(password)}`,
+    method: 'get'
+  }))
+
+  useEffect(() => {
+    if (user?.data) {
+      if (user.data.length > 0) {
+        setLoginFailed(false)
+        dispatch({ type: 'LOGIN', username: user.data[0].username })
+        return
+      }
+
+      setLoginFailed(true)
+    }
+
+    user?.error &&
+    setLoginFailed(true)
+  }, [user])
 
   const handleUsername = e => {
     setUsername(e.target.value)
   }
 
+  const handlePassword = e => {
+    setPassword(e.target.value)
+  }
+
   const handleSubmit = e => {
     e.preventDefault()
-    dispatch({ type: 'LOGIN', username })
+    login(username, password)
   }
 
   return (
@@ -26,13 +52,22 @@ const Login = () => {
       />
 
       <label htmlFor="login-password">Password</label>
-      <input type="password" name="login-password" id="login-password" />
+      <input
+        type="password"
+        name="login-password"
+        id="login-password"
+        value={password}
+        onChange={handlePassword}
+      />
 
       <input
         type="submit"
         value="Login"
         disabled={username.length === 0}
       />
+
+      {loginFailed &&
+      <span style={{ color: 'red' }}>Invalid username or password</span>}
     </form>
   )
 }
