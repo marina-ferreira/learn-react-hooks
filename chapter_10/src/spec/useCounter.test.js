a9168e7f6274cf6f0b3a80a45cf409ac023e91b6
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-06-27 06:40:03 -0300

    Chapter 10 | useCounter hook result test

---
+++ chapter_10/src/spec/useCounter.test.js
@@ -0,0 +1,10 @@
+import { renderHook, act } from '@testing-library/react-hooks'
+
+import useCounter from 'hooks/useCounter'
+
+test('should use counter', () => {
+  const { result } = renderHook(() => useCounter())
+
+  expect(result.current.count).toBe(0)
+  expect(typeof result.current.increment).toBe('function')
+})
