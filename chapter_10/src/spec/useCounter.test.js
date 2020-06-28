376ab31cca87e03bcba7d938b0b16e8c714314d8
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-06-28 05:24:07 -0300

    Chapter 10 | useCounter hook increment test

--- chapter_10/src/spec/useCounter.test.js
+++ chapter_10/src/spec/useCounter.test.js
@@ -8,3 +8,19 @@ test('should use counter', () => {
   expect(result.current.count).toBe(0)
   expect(typeof result.current.increment).toBe('function')
 })
+
+test('should increment counter', () => {
+  const { result } = renderHook(() => useCounter())
+
+  act(() => result.current.increment())
+
+  expect(result.current.count).toBe(1)
+})
+
+test('should use initial value', () => {
+  const { result } = renderHook(() => useCounter(123))
+
+  expect(result.current.count).toBe(123)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(124)
+})
