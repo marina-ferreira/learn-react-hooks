64e8222460a9a5b38a462d8353d5b52d395b2df3
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-06-29 06:19:32 -0300

    Chapter 10 | useDebouncedUndo hook test

---
+++ chapter_10/src/spec/hooks/useDebouncedUndo.test.js
@@ -0,0 +1,40 @@
+import { renderHook, act } from '@testing-library/react-hooks'
+
+import { useDebouncedUndo } from 'hooks'
+
+test('should use debounce undo', () => {
+  const { result } = renderHook(() => useDebouncedUndo())
+  const [content, setter, undoRest] = result.current
+
+  expect(content).toBe('')
+  expect(typeof setter).toBe('function')
+  expect(typeof undoRest.undo).toBe('function')
+  expect(typeof undoRest.redo).toBe('function')
+  expect(undoRest.canUndo).toBe(false)
+  expect(undoRest.canRedo).toBe(false)
+})
+
+test('should update content immediately', () => {
+  const { result } = renderHook(() => useDebouncedUndo())
+  const [content, setter, undoRest] = result.current
+
+  expect(content).toBe('')
+  act(() => setter('test'))
+
+  const [newContent] = result.current
+  expect(newContent).toBe('test')
+})
+
+test('should debounce undo history update', async () => {
+  const { result, waitForNextUpdate } = renderHook(() => useDebouncedUndo())
+  const [, setter] = result.current
+  act(() => setter('test'))
+
+  const [, , undoRest] = result.current
+  expect(undoRest.canUndo).toBe(false)
+
+  await act(async () => await waitForNextUpdate())
+
+  const [, , newUndoRest] = result.current
+  expect(newUndoRest.canUndo).toBe(true)
+})
