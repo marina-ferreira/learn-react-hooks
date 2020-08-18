87336f87fe7611f99781e977365a15f3ecd388c9
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-08-18 08:40:30 -0300

    Chapter 11 | Adds toggle check and remove handlers to todo itme

--- chapter_11/todo-class-components/src/components/TodoItem.js
+++ chapter_11/todo-class-components/src/components/TodoItem.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react'
 
 class TodoItem extends Component {
+  constructor(props) {
+    super(props)
+
+    this.handleToggle = this.handleToggle.bind(this)
+    this.handleRemove = this.handleRemove.bind(this)
+  }
+
+  handleToggle() {
+    const { id, toggleTodo } = this.props
+    toggleTodo(id)
+  }
+
+  handleRemove() {
+    const { id, removeTodo } = this.props
+    removeTodo(id)
+  }
+
   render() {
     const { title, completed } = this.props
 
     return (
       <div>
-        <input type="checkbox" checked={completed} />
+        <input type="checkbox" checked={completed} onChange={this.handleToggle} />
         {title}
-        <button>x</button>
+        <button onClick={this.handleRemove}>x</button>
       </div>
     )
   }
