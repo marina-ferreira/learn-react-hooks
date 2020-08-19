2544b917aec1e49d070b7f9ee7ffd5206bb5a39e
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-08-19 07:50:34 -0300

    Chapter 12 | Switches useReducer hook for redux in app component

--- chapter_12/redux/src/App.js
+++ chapter_12/redux/src/App.js
@@ -1,16 +1,26 @@
-import React, { useEffect, useReducer, useMemo } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
+import { createStore } from 'redux'
 
 import StateContext from 'contexts/StateContext'
 import { fetchAPITodos } from 'services/api'
-import appReducer from 'reducers'
+import appReducer from 'store/reducersducers'
 
 import Header from 'components/Header'
 import AddTodo from 'components/AddTodo'
 import TodoList from 'components/TodoList'
 import TodoFilter from 'components/TodoFilter'
 
+const initialState = { todos: [], filter: 'all' }
+const store = createStore(appReducer, initialState)
+const { dispatch } = store
+
 const App = () => {
-  const [state, dispatch] = useReducer(appReducer, { todos: [], filter: 'all' })
+  const [state, setState] = useState(initialState)
+
+  useEffect(() => {
+    const unsubscribe = store.subscribe(() => setState(store.getState()))
+    return unsubscribe
+  }, [])
 
   useEffect(() => {
     fetchAPITodos().then(todos => {
