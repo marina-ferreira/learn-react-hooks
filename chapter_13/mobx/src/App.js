b4f66ed321da56c9315a5f3b3b537268460d6f9e
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-08-22 07:19:03 -0300

    Chapter 13 - Mobx | Connects app component to store

--- chapter_13/mobx/src/App.js
+++ chapter_13/mobx/src/App.js
@@ -1,64 +1,24 @@
-import React, { useEffect, useReducer, useMemo } from 'react'
-
-import StateContext from 'contexts/StateContext'
-import { fetchAPITodos } from 'services/api'
-import appReducer from 'reducers'
+import React, { useEffect } from 'react'
+import { inject } from 'mobx-react'
 
 import Header from 'components/Header'
 import AddTodo from 'components/AddTodo'
 import TodoList from 'components/TodoList'
 import TodoFilter from 'components/TodoFilter'
 
-const App = () => {
-  const [state, dispatch] = useReducer(appReducer, { todos: [], filter: 'all' })
-
+const App = ({ todoStore }) => {
   useEffect(() => {
-    fetchAPITodos().then(todos => {
-      dispatch({ type: 'FETCH_TODOS', todos })
-    })
-  }, [])
-
-  const filteredTodos = useMemo(() => {
-    const { filter, todos } = state
-
-    switch (filter) {
-      case 'active':
-        return todos.filter(todo => !todo.completed)
-      case 'completed':
-        return todos.filter(todo => todo.completed)
-
-      default:
-      case 'all':
-        return todos
-    }
-  }, [state])
-
-  const toggleTodo = id => {
-    dispatch({ type: 'TOGGLE_TODO', id })
-  }
-
-  const addTodo = title => {
-    dispatch({ type: 'ADD_TODO', title })
-  }
-
-  const removeTodo = id => {
-    dispatch({ type: 'REMOVE_TODO', id })
-  }
-
-  const filterTodos = filter => {
-    dispatch({ type: 'FILTER_TODOS', filter })
-  }
+    todoStore.fetch()
+  }, [todoStore])
 
   return (
-    <StateContext.Provider value={filteredTodos}>
       <div>
         <Header />
-        <AddTodo addTodo={addTodo} />
-        <TodoList toggleTodo={toggleTodo} removeTodo={removeTodo} />
-        <TodoFilter filter={state.filter} filterTodos={filterTodos} />
+        <AddTodo />
+        <TodoList />
+        <TodoFilter />
       </div>
-    </StateContext.Provider>
   )
 }
 
-export default App
+export default inject('todoStore')(App)
