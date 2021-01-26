91052f1e903a5230799b90354192d83dddb69c0b
Marina Ferreira <marina.ferreira.developer@gmail.com>
2021-01-26 12:54:50 -0300

    Updates setInterval call

--- chapter_10/src/hook-components/UseInterval.js
+++ chapter_10/src/hook-components/UseInterval.js
@@ -23,7 +23,7 @@ export default UseInterval
 const [count, setCount] = useState(0)
 
 useEffect(() => {
-  const interval = setInterval(() => setCount(count + 1), 1000)
+  const interval = setInterval(() => setCount(count => count + 1), 1000)
   return () => clearInterval(interval)
 })
 */
